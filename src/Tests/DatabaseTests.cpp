62a5e7a2611f2dbba366d904b879e486db7044fd
Juan Manuel Hidalgo <juanma06@gmail.com>
2016-04-11 00:59:00 -0300

    agregado FindRocksDB.cmake

--- src/Tests/DatabaseTests.cpp
+++ src/Tests/DatabaseTests.cpp
@@ -36,5 +36,32 @@ TEST(DatabaseTests,TestSaveUser){
 }
 
 
+TEST(DatabaseTests,TestSaveAndGetUser){
+	Database* database = new Database();
+	User* user = new User("User1");
+	user->setPassword("hola");
+	user->setName("Juan");
+	user->setLocationX(20.0);
+	user->setLocationY(40.0);
+	user->setPerfilImage("URL");
+	database->saveUser(user);
+	ASSERT_EQ(user->getName(), database->getUser(user->getUsername())->getName());
+	delete user;
+	delete database;
+}
+
+/*TEST(DatabaseTests,TestSaveAndGetMessage){
+	Database* database = new Database();
+	User* user1 = new User("User1");
+	User* user2 = new User("User2");
+	string data = "hola";
+	Message* message = new Message(user1, user2, data);
+	message->setId(1);
+	database->saveMessage(message);
+	ASSERT_EQ(data, database->getMessage(user1,user2, ));
+	delete user;
+	delete database;
+}*/
+
 
 
