dd1cab88b751ee3d0a46c51450ef4752a82d6004
juanmahidalgo <juanmahidalgo@MacBook-Air-de-Juan.local>
2016-05-04 02:10:15 -0300

    cambios en el docker

--- Docker/docker-entrypoint.sh
+++ Docker/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone -b registerAndLogin https://github.com/bliberini/7552-TDPII
+git clone https://github.com/bliberini/7552-TDPII
 echo `ls`
 cd 7552-TDPII
 mkdir build-dir
@@ -13,9 +13,5 @@ cd -
 cd 7552-TDPII/build-dir
 cmake ..
 make
-
-lcov --no-external --capture --initial --directory /7552-TDPII/build-dir/ --output-file /7552-TDPII/build-dir/proy_base.info
-./tests
-lcov --capture --directory /7552-TDPII/build-dir/ --output-file /7552-TDPII/build-dir/proy_tests.info
-lcov --add-tracefile /7552-TDPII/build-dir/proy_base.info --add-tracefile /7552-TDPII/build-dir/proy_tests.info  --output-file /7552-TDPII/build-dir/proy_total.info
-lcov --remove /7552-TDPII/build-dir/proy_tests.info '/usr/include/' '/usr/lib/*' '/7552-TDPII/src/mongoose.c' '/usr/local/include/*' '4.8/*' 'src/json/json.h' 'include/gtest/*' -o /7552-TDPII/build-dir/proy_tests.info
+cd ..
+sh scriptCoverage.sh
