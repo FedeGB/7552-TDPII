cc164f44ed4860a2535cdd7918ea9f157f723bb9
fedegb <fedeagb@gmail.com>
2016-05-26 00:44:41 -0300

    Hace response generica con pasa manos de lo que viene del Shared Server de los 10 primeros usuarios. #23

--- src/HttpEvents/GetCandidateEvent.cpp
+++ src/HttpEvents/GetCandidateEvent.cpp
@@ -17,7 +17,20 @@ GetCandidateEvent::~GetCandidateEvent() {
 void GetCandidateEvent::handle(Manager* manager, SharedManager* sManager) {
 	bool validation = this->validateInput();
 	if(validation) {
-		
+		Json::Value sharedUsers = sManager->getUsers();
+		Json::ValueConstIterator it = sharedUsers.begin();
+		int max = 10;
+		int act = 0;
+		Json::Value returnCandidates = Json::Value();
+		Json::Value candidates = Json::Value(Json::arrayValue);
+		returnCandidates["candidates"] = candidates;
+		while(it != sharedUsers.end() && act < max) {
+		    const Json::Value& user = *it;
+		    returnCandidates["candidates"].append(user);
+			it++;
+			act++;
+		}
+		this->response(0, "", returnCandidates);
 	}
 }
 
