c27933ee80b8fb52fa692aecec03b14dc508e826
juanmahidalgo <juanma06@gmail.com>
2016-06-23 01:03:38 -0300

    mas tests de la API

--- APITests.py
+++ APITests.py
@@ -7,17 +7,16 @@ class TestAPI(unittest.TestCase):
         self.apiURL = "http://localhost:8003"
         self.loginURL = "/login"
         self.usersURL = "/users"
+	self.messagesURL = "/messages"
 
 
 
     def test_createUser(self):
         dictDelete = "{\"username\":\"juanmahid\"}"
         r = requests.delete(self.apiURL+self.usersURL, data=dictDelete)
-	print r.json()
         dict = "{\"username\" : \"juanmahid\", \"password\" : \"hola\", \"name\" : \"juanmahid\", \"sex\": \"M\" }"
         r = requests.post(self.apiURL+self.usersURL, data=dict)
         json = r.json()
-	print json
         self.assertEqual("Registered" , json["message"])
         r = requests.delete(self.apiURL+self.usersURL, data=dictDelete)
         json = r.json()
@@ -64,7 +63,43 @@ class TestAPI(unittest.TestCase):
         json = r.json()
         self.assertGreater(json["payload"]["Users"].__len__() , 3)
 
+    def test_saveMessage(self):
+        dict = "{\"username\" : \"juanmahid\", \"password\" : \"hola\", \"name\" : \"juanma\" }"
+        requests.post(self.apiURL+self.usersURL, data=dict)
+	dict = "{\"username\":\"pepe\",\"password\":\"hola\",\"name\":\"pepe\"}"
+        requests.post(self.apiURL+self.usersURL, data=dict)
+	message = "{\"user1\":\"juanmahid\", \"user2\":\"pepe\", \"data\":\"holapepe\" } "
+	r = requests.get(self.apiURL + "/users/login", headers={'Authorization': 'Basic anVhbm1haGlkOmhvbGE='})
+        json = r.json()
+        self.assertEqual("OK" , json["payload"]["result"])
+	token = json["payload"]["token"]
+	r = requests.post(self.apiURL + self.messagesURL , data=message, headers={'Token': token})
+	json = r.json()
+	self.assertEqual("Message Saved" , json["message"])
 
+    def test_getAllMessages(self):
+        dict = "{\"username\" : \"juanmahid\", \"password\" : \"hola\", \"name\" : \"juanma\" }"
+        requests.post(self.apiURL+self.usersURL, data=dict)
+	dict = "{\"username\":\"pepe\",\"password\":\"hola\",\"name\":\"pepe\"}"
+        requests.post(self.apiURL+self.usersURL, data=dict)
+	message = "{\"user1\":\"juanmahid\", \"user2\":\"pepe\", \"data\":\"holapepe\" } "
+	r = requests.get(self.apiURL + "/users/login", headers={'Authorization': 'Basic anVhbm1haGlkOmhvbGE='})
+        json = r.json()
+        self.assertEqual("OK" , json["payload"]["result"])
+	token = json["payload"]["token"]
+	r = requests.post(self.apiURL + self.messagesURL , data=message, headers={'Token': token})
+	message = "{\"user1\":\"juanmahid\", \"user2\":\"pepe\", \"data\":\"como andas?\" } "
+	r = requests.post(self.apiURL + self.messagesURL , data=message, headers={'Token': token})
+	message = "{\"user1\":\"juanmahid\", \"user2\":\"pepe\", \"data\":\"yo todo bien\" } "
+	r = requests.post(self.apiURL + self.messagesURL , data=message, headers={'Token': token})
+	message = "{\"user1\":\"juanmahid\", \"user2\":\"pepe\", \"data\":\"chau\" } "
+	r = requests.post(self.apiURL + self.messagesURL , data=message, headers={'Token': token})
+	json = r.json()
+	self.assertEqual("Message Saved" , json["message"])
+	r = requests.get(self.apiURL + "/messages", headers={'Token': token})
+	json = r.json()
+	messages = json["payload"]["messages"]
+	self.assertGreater(messages.__len__() , 3)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestAPI)
